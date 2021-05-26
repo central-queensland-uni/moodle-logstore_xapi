ebb0b4f6065dd33aaf05e167eec3226782f55893
ScottVerbeek <scottverbeek@catalyst-au.net>
2021-05-26 16:04:02 +1000

    Added transformer for template_viewed event from mod_spa

--- src/transformer/get_event_function_map.php
+++ src/transformer/get_event_function_map.php
@@ -99,6 +99,7 @@ function get_event_function_map() {
         '\mod_spa\event\template_created' => 'mod_spa\template_created',
         '\mod_spa\event\template_deleted' => 'mod_spa\template_deleted',
         '\mod_spa\event\template_updated' => 'mod_spa\template_updated',
+        '\mod_spa\event\template_viewed' => 'mod_spa\template_viewed',
         '\mod_spa\event\template_questions_added' => 'mod_spa\template_questions_added',
         '\mod_spa\event\template_questions_viewed' => 'mod_spa\template_questions_viewed',
         '\mod_survey\event\course_module_viewed' => 'mod_survey\course_module_viewed',
