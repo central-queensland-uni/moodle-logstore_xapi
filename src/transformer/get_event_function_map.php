09767860e124e8eab5a3cd7af43b335e6b3733ab
ScottVerbeek <scottverbeek@catalyst-au.net>
2021-05-26 13:22:45 +1000

    Added transformer for submission_reviewed event from mod_spa

--- src/transformer/get_event_function_map.php
+++ src/transformer/get_event_function_map.php
@@ -88,6 +88,7 @@ function get_event_function_map() {
         '\mod_spa\event\question_previewed' => 'mod_spa\question_previewed',
         '\mod_spa\event\question_viewed' => 'mod_spa\question_viewed',
         '\mod_spa\event\submission_created' => 'mod_spa\submission_created',
+        '\mod_spa\event\submission_reviewed' => 'mod_spa\submission_reviewed',
         '\mod_spa\event\submission_viewed' => 'mod_spa\submission_viewed',
         '\mod_survey\event\course_module_viewed' => 'mod_survey\course_module_viewed',
         '\mod_url\event\course_module_viewed' => 'mod_url\course_module_viewed',
